f7b92fd5adf1021361f5b2cf349e71e1d79a5994
Gaspard Deremble <g.deremble@laposte.net>
2025-09-14 02:32:05 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zed-\lprog
+
+Language support for the \lprog language.
+
+Tree sitter grammar: [https://github.com/Gaspartcho/tree-sitter-lprog]
---
+++ languages/lprog/highlights.scm
@@ -0,0 +1,21 @@
+
+(comment) @comment
+
+(variable) @variable
+(macro) @constant
+
+
+(directive) @keyword
+
+(function variable: (variable) @function)
+(function "\\" @operator)
+(function "."  @operator)
+
+
+(string) @string
+
+
+["(" ")"] @punctuation.bracket
+
+
+"," @punctuation
